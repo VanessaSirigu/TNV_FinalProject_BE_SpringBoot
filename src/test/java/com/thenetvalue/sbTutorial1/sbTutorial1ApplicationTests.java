313f8be9432d4c5412912d2324f0a7e833bfcfdc
Vanessa <vane.sirigu@gmail.com>
2021-10-10 14:30:45 +0200

    Configurazione iniziale progetto base con inMemoryDAO

---
+++ src/test/java/com/thenetvalue/sbTutorial1/sbTutorial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thenetvalue.sbTutorial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class sbTutorial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
